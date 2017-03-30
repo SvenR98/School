ad034955bf8d99a2354a4d237b7c0a246268c965
SvenR98 <srolleman01@student.rocvantwente.nl>
2017-03-30 14:33:57 +0200

    Taak 6 toegevoegd

---
+++ GrafischeRekenmachine-Taak 6/GrafischeRekenmachine.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="GrafischeRekenmachine.aspx.cs" Inherits="GrafischeRekenmachine" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        De Rekenmachine<br />
+        <br />
+        Hieronder staat een Grafische rekenmachine. Gebruik de onderstaande knoppen om deze te gebruiken.<br />
+        <br />
+        <asp:TextBox ID="txtScherm" runat="server" Height="46px" Width="356px" Font-Size="XX-Large" OnTextChanged="txtScherm_TextChanged"></asp:TextBox>
+        &nbsp; <asp:Label ID="lblControleren" runat="server" Text="Kan niet met een 0 Beginnen!" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <asp:Button ID="btn1" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn1_Click" Text="1" Width="65px" />
+&nbsp;<asp:Button ID="btn2" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn2_Click" Text="2" Width="65px" />
+&nbsp;<asp:Button ID="btn3" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn3_Click" Text="3" Width="65px" />
+&nbsp;<asp:Button ID="btnClear" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="C" Width="65px" OnClick="btnClear_Click" />
+&nbsp;<asp:Button ID="btnDelete" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="Back" Width="65px" OnClick="btnDelete_Click" />
+        <br />
+        <br />
+        <asp:Button ID="btn4" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn4_Click" Text="4" Width="65px" />
+&nbsp;<asp:Button ID="btn5" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn5_Click" Text="5" Width="65px" />
+&nbsp;<asp:Button ID="btn6" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn6_Click" Text="6" Width="65px" />
+&nbsp;<asp:Button ID="btnPlus" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="+" Width="65px" OnClick="btnPlus_Click" />
+&nbsp;<asp:Button ID="Button10" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="M1" Width="65px" OnClick="Button10_Click" />
+        <br />
+        <br />
+        <asp:Button ID="btn7" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn7_Click" Text="7" Width="65px" />
+&nbsp;<asp:Button ID="btn8" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn8_Click" Text="8" Width="65px" />
+&nbsp;<asp:Button ID="btn9" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn9_Click" Text="9" Width="65px" />
+&nbsp;<asp:Button ID="btnMinus" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="-" Width="65px" OnClick="btnMinus_Click" />
+&nbsp;<asp:Button ID="Button15" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="O1" Width="65px" />
+        <br />
+        <br />
+        <asp:Button ID="btn0" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" OnClick="btn0_Click" Text="0" Width="138px" />
+&nbsp;<asp:Button ID="btnPunt" runat="server" BorderWidth="1px" Font-Size="Larger" Height="60px" Text="." Width="65px" OnClick="btnPunt_Click" />
+&nbsp;<asp:Button ID="btnEquals" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="=" Width="65px" OnClick="btnEquals_Click" />
+&nbsp;<asp:Button ID="Button19" runat="server" BorderWidth="1px" Font-Size="Large" Height="60px" Text="O2" Width="65px" />
+        <br />
+        <asp:Label ID="lblGetal1" runat="server" Text="Getal1" Visible="False"></asp:Label>
+&nbsp;<asp:Label ID="lblBereken" runat="server" Text="Label" Visible="False"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ GrafischeRekenmachine-Taak 6/GrafischeRekenmachine.aspx.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class GrafischeRekenmachine : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btn1_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "1";
+        } 
+    }
+
+    protected void btn2_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "2";
+        }
+    }
+
+    protected void btn3_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "3";
+        }
+    }
+
+    protected void btn4_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "4";
+        }
+    }
+
+    protected void btn5_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "5";
+        }
+    }
+
+    protected void btn6_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "6";
+        }
+    }
+
+    protected void btn7_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "7";
+        }
+    }
+
+    protected void btn8_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "8";
+        }
+    }
+
+    protected void btn9_Click(object sender, EventArgs e)
+    {
+        lblControleren.Visible = false;
+        if (txtScherm.Text.Length < 7)
+        {
+            txtScherm.Text += "9";
+        }
+    }
+
+    protected void btn0_Click(object sender, EventArgs e)
+    {
+        
+        
+            if (txtScherm.Text.Length < 7)
+            {
+                if (txtScherm.Text == "")
+                {
+                    txtScherm.Text += "0";
+                }
+                else if (txtScherm.Text.Contains(","))
+                {
+                    txtScherm.Text += "0";
+                }
+                else
+                {
+                txtScherm.Text += "";
+            }
+        }
+ 
+        
+    }
+
+    protected void btnPlus_Click(object sender, EventArgs e)
+    {
+        lblGetal1.Text = txtScherm.Text;
+        txtScherm.Text = string.Empty;
+        lblBereken.Text = "+";
+    }
+
+    protected void btnPunt_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Contains(",") != true)
+        {
+            if (txtScherm.Text.Length == 0)
+            {
+                txtScherm.Text += "0,";
+            }
+            else
+            {
+                txtScherm.Text += ",";
+            }
+        }
+    }
+
+    protected void btnEquals_Click(object sender, EventArgs e)
+    {
+        btnDelete.Enabled = false;
+        //Maak de variabelen
+        double G1 = Convert.ToDouble(txtScherm.Text);
+        double G2 = Convert.ToDouble(lblGetal1.Text);
+        double Uitkomst = 0;
+        // Maak de berekening
+        if (lblBereken.Text == "+")
+        {
+            Uitkomst = G2 + G1;
+        }
+        else if (lblBereken.Text == "-")
+        {
+            Uitkomst = G2 - G1;
+        }
+        txtScherm.Text = Convert.ToString(Uitkomst);
+
+    }
+
+    protected void btnMinus_Click(object sender, EventArgs e)
+    {
+        lblGetal1.Text = txtScherm.Text;
+        txtScherm.Text = string.Empty;
+        lblBereken.Text = "-";
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        txtScherm.Text = string.Empty;
+        lblGetal1.Text = string.Empty;
+        btnDelete.Enabled = true;
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        if (txtScherm.Text.Length > 0)
+        {
+            txtScherm.Text = txtScherm.Text.Remove(txtScherm.Text.Length - 1);
+        }
+    }
+
+    protected void txtScherm_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+
+    }
+}
